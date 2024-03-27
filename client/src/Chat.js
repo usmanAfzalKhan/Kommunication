eab491765b0c048ac0422a57f7c643756f616a26
Usman Afzal Khan <usmanafzalkhan01@gmail.com>
2024-03-27 03:11:34 -0400

    feat: clear input once message is sent

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ScrollToBottom from 'react-scroll-to-bottom'
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -18,6 +19,7 @@ function Chat({ socket, username, room }) {
 
       await socket.emit("send_message", messageData);
       setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
@@ -33,28 +35,37 @@ function Chat({ socket, username, room }) {
         <p>Live Chat</p>
       </div>
       <div className="chat-body">
+        <ScrollToBottom className="message-container">
         {messageList.map((messageContent) => {
           return (
-            <div className="message" id={username === messageContent.author ? "you" : "other"}>
+            <div
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
+            >
               <div>
                 <div className="message-content">
-                    <p>{messageContent.message}</p>
+                  <p>{messageContent.message}</p>
                 </div>
                 <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
                 </div>
               </div>
             </div>
           );
         })}
+        </ScrollToBottom>
       </div>
       <div className="chat-footer">
         <input
           type="text"
+          value={currentMessage}
           placeholder="Send Message..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
           }}
         />
         <button onClick={sendMessage}>&#9658;</button>

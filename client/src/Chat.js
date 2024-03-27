ef69bb1a046b75da3e2bf418ab41cfd71dbe9807
Usman Afzal Khan <usmanafzalkhan01@gmail.com>
2024-03-27 02:54:44 -0400

    feat: message styles and logic

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
-  
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -11,27 +11,44 @@ function Chat({ socket, username, room }) {
         author: username,
         message: currentMessage,
         time:
-          new Date(Date.now()).getHours() + 
-          ":" + 
+          new Date(Date.now()).getHours() +
+          ":" +
           new Date(Date.now()).getMinutes(),
       };
 
       await socket.emit("send_message", messageData);
-    } 
+      setMessageList((list) => [...list, messageData]);
+    }
   };
 
-useEffect(() => {
-    socket.on("recieve_message", (data) => { 
-         console.log(data);
-    })
-}, [socket]); 
+  useEffect(() => {
+    socket.on("recieve_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
   return (
     <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        {messageList.map((messageContent) => {
+          return (
+            <div className="message" id={username === messageContent.author ? "you" : "other"}>
+              <div>
+                <div className="message-content">
+                    <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <div className="chat-footer">
         <input
           type="text"
